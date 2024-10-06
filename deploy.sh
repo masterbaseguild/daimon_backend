236dd0da69e80133167978b788e7c6d25f6395f4
mbentity <dariobizzarri02@gmail.com>
2024-10-06 13:44:34 +0200

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
-cd /opt/daimon_backend
-git checkout live
-git pull
-docker build -t daimon_backend .
-docker-compose up -d
+#!/bin/sh
+
+cd /opt/daimon_backend || exit
+
+git fetch origin
+
+if [ $(git rev-list HEAD...origin/live --count) -gt 0 ]; then
+    echo "New updates found, pulling changes..."
+    git checkout live
+    git pull
+    docker build -t daimon_backend .
+    docker-compose up -d
+else
+    echo "No new updates."
+fi
