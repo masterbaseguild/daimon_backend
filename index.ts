5edaf2e0294b35c19b3f963685c615f1e1470c1b
mbentity <dariobizzarri02@gmail.com>
2025-05-08 23:06:20 +0200

    rollback localhost allow in cors

--- index.ts
+++ index.ts
@@ -218,7 +218,7 @@ passport.use("minecraft", new localStrategy({passReqToCallback: true}, async (re
 
 const app = express();
 const corsOptions: cors.CorsOptions = {
-    origin: [process.env.FRONTEND_ENDPOINT || "", process.env.BACKEND_ENDPOINT || "", process.env.WEBSITE_ENDPOINT || ""],
+    origin: [process.env.FRONTEND_ENDPOINT || "", process.env.BACKEND_ENDPOINT || "", process.env.WEBSITE_ENDPOINT || "", "http://localhost:4000", "http://localhost"],
     credentials: true
 };
 app.use(cors(corsOptions));
